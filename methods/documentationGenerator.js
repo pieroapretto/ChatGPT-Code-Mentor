3cdb46e38338a85cd2607e09c3db41a5dd03cf3d
Piero Pretto <piero@copilotsearch.com>
2023-04-04 23:01:43 -0500

    update documentationGenerator method to write multilines

--- methods/documentationGenerator.js
+++ methods/documentationGenerator.js
@@ -4,16 +4,16 @@ const config = require('dotenv').config();
 async function documentationGenerator(input) {
   try {
     const res = await tectalicOpenai(process.env.OPENAI_API_KEY)
-    .completions.create({
-      model: 'text-davinci-003',
-      prompt: 'Write documentation for the following JavaScript code: ' + input,
-    })
+    .chatCompletions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: 'Write documentation for the following JavaScript code: ' + input }]
+    });
 
-    const documentation = res.data.choices[0].text.trim();
+    const cypress_test_recommendations = res.data.choices[0].message.content.trim();
 
-    console.log(`\n${documentation}`);
+    console.log(`\n${cypress_test_recommendations}`);
 
-    return `Documentation suggestion for this pull request:\n\`\`\`diff\n${documentation}\n\`\`\``;
+    return `Cypress tests suggestion for this pull request:\n\`\`\`diff\n${cypress_test_recommendations}\n\`\`\``;
 
   } catch (err) {
     if (err?.response) {

84f03e9ec4f373ea4d4fb014d5bdcf930881f296
Piero Pretto <piero@copilotsearch.com>
2023-04-11 21:24:44 -0500

    lets try something..

--- pull-request-automations.js
+++ pull-request-automations.js
@@ -1,6 +1,52 @@
 const axios = require('axios');
 const fs = require("fs");
-const postComment = require('./github-api/postComment.js');
+
+const generateMarkdown = require('./utils/markdown-generation.js');
+// Import methods supports by tectalicOpenai
+const { chatCompletion } = require('./methods/chatGenerator.js');
+const { documentationGenerator } = require('./methods/documentationGenerator.js');
+const { cypressTestsGenerator } = require('./methods/cypressTestsGenerator.js');
+
+const postComment = async (comment_type, token, owner, repo, pr_number, pr_diff, generate_markdown=false) => {
+  let comment_payload = null;
+
+  try {
+    switch (comment_type) {
+      case 'documentation':
+        comment_payload = await documentationGenerator(pr_diff, 'Write documentation for this git diff');
+        break;
+      case 'cypress':
+        comment_payload = await cypressTestsGenerator(pr_diff, 'Write Cypress tests for this PR diff');
+        break;
+      case 'chat':
+      default:
+        comment_payload = await chatCompletion(pr_diff);
+    }
+
+  } catch (error) {
+    console.error(`An error occurred with ${comment_type} postComment comment payload generation`, error);
+    return;
+  }
+
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://api.github.com/repos/${owner}/${repo}/issues/${pr_number}/comments`,
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/vnd.github+json',
+      },
+      data: {
+        body: generate_markdown ? generateMarkdown(comment_payload) : comment_payload
+      },
+    });
+
+    console.log(`Github omment posted successfully: ${response.data.html_url}`);
+  } catch (error) {
+    console.error(`Failed to post Github comment: ${error.message}`);
+    return error;
+  }
+}
 
 const main = async () => {
   // Read the content of the pr_diff.txt file
@@ -11,8 +57,8 @@ const main = async () => {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
   const pr_number = process.env.PR_NUMBER;
 
-  // // Post the comment to the PR with documentation suggestions
-  // await postComment('documentation', token, owner, repo, pr_number, pr_diff, true);
+  // Post the comment to the PR with documentation suggestions
+  await postComment('documentation', token, owner, repo, pr_number, pr_diff, true);
   // Post the comment to the PR with cypress tests suggestions
   await postComment('cypress', token, owner, repo, pr_number, pr_diff);
 };
